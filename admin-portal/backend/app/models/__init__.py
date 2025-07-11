c2d296c762899b53db606491bd1d4883bd48b463
JahBless930 <aduquue14@gmail.com>
2025-07-11 15:00:25 +0000

    Update __init__.py

--- admin-portal/backend/app/models/__init__.py
+++ admin-portal/backend/app/models/__init__.py
@@ -1,5 +1,6 @@
-from .base import Base
+from .base import Base, BaseModel
 from .user import User
 from .project import Project
+from .project_figure import ProjectFigure
 
-__all__ = ["Base", "User", "Project"]
+__all__ = ["Base", "BaseModel", "User", "Project", "ProjectFigure"]
