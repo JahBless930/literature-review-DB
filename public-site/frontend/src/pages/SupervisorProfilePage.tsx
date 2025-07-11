73e5cd44b912e1b5e6daf321bc156933dc096df3
JahBless930 <aduquue14@gmail.com>
2025-07-11 16:53:55 +0000

    Update SupervisorProfilePage.tsx

--- public-site/frontend/src/pages/SupervisorProfilePage.tsx
+++ public-site/frontend/src/pages/SupervisorProfilePage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import Layout from '../components/Layout';
-import { api } from '../services/api';
+import { apiService } from '../services/api';
 import { UserProfile, ProjectSummary } from '../types';
 
 export const SupervisorProfilePage: React.FC = () => {
